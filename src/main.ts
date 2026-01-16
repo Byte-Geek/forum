49baed9a4d8b186974adba71af04612f162f1122
Babuin4ikS <bytegeek33@gmail.com>
2026-01-16 16:47:40 +0200

    f

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['https://forum-production-b9a9.up.railway.app'],
+    origin: ['https://forum-production-b9a9.up.railway.app', 'http://localhost:3000', 'http://localhost:3001', 'https://localhost:3000', 'https://localhost:3001'],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
